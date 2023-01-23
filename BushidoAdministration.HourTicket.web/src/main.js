af1e7f05e8ed3e1a8962aba06f7698a94595d553
Tocco17 <fede.ruocco@gmail.com>
2023-01-23 11:51:27 +0100

    Pinia settings
    #a85b2e9

--- BushidoAdministration.HourTicket.web/src/main.js
+++ BushidoAdministration.HourTicket.web/src/main.js
@@ -3,10 +3,11 @@ import { createApp } from 'vue'
 import './input.css'
 import App from './App.vue'
 import router from './plugins/router'
-
+import pinia from './plugins/pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
