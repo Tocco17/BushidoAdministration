1acc6ce4dc3ca801f1f67f1ec4c3fc07491a4039
Tocco17 <fede.ruocco@gmail.com>
2023-01-23 10:53:50 +0100

    Config tailwind

--- BushidoAdministration.HourTicket.web/src/main.js
+++ BushidoAdministration.HourTicket.web/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+import './input.css'
 import App from './App.vue'
 import router from './plugins/router'
+
 
 const app = createApp(App)
 
