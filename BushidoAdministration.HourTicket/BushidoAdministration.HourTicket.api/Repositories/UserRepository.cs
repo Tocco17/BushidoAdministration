15bd73021d339959ad7be770c74e722edaa164df
FedericoRuocco <federico.ruocco@giuneco.it>
2023-02-03 14:31:12 +0100

    Edit query in user repository
    Centralized the table field

--- BushidoAdministration.HourTicket/BushidoAdministration.HourTicket.api/Repositories/UserRepository.cs
+++ BushidoAdministration.HourTicket/BushidoAdministration.HourTicket.api/Repositories/UserRepository.cs
@@ -9,6 +9,8 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 	{
 		private readonly IContext _context;
 
+		private readonly string _table = "dbo.users";
+
 		private readonly string _id = " id as Id ";
 		private readonly string _email = " email as Email ";
 		private readonly string _username = " username as Username ";
@@ -26,7 +28,7 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 		{
 			var query = "select top(1) " +
 				$"{_id}, {_email}, {_username}, {_password}, {_firstName}, {_lastName}, {_roleLevel} " +
-				"from dbo.users " +
+				$"from {_table} " +
 				$"where email = '{email}' and password = '{password}'";
 
 			var user = await _context.GetSingleAsync<User>(query);
@@ -38,7 +40,7 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 		{
 			var query = "select top(1) " +
 				$"{_id}, {_email}, {_username}, {_password}, {_firstName}, {_lastName}, {_roleLevel} " +
-				"from dbo.users " +
+				$"from {_table} " +
 				$"where username = '{username}' and password = '{password}'";
 
 			var user = await _context.GetSingleAsync<User>(query);
@@ -48,7 +50,7 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 
 		public async Task<RoleLevel?> GetRoleLevel(int userId)
 		{
-			var query = $"select top(1) {_roleLevel} from dbo.users where id = {userId}";
+			var query = $"select top(1) {_roleLevel} from {_table} where id = {userId}";
 			return await _context.GetSingleAsync<RoleLevel?>(query);
 		}
 
@@ -56,7 +58,7 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 		{
 			var query = $"select top(1) " +
 				$"{_id}, {_email}, {_username}, {_password}, {_firstName}, {_lastName}, {_roleLevel} " +
-				$"from dbo.users where id = {userId}";
+				$"from {_table} where id = {userId}";
 			var user = await _context.GetSingleAsync<User>(query);
 			return user;
 		}
@@ -65,7 +67,7 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 		{
 			var query = $"select top(1) " +
 				$"{_id}, {_email}, {_username}, {_password}, {_firstName}, {_lastName}, {_roleLevel} " +
-				$"from dbo.users where email = '{email}'";
+				$"from {_table} where email = '{email}'";
 			var user = await _context.GetSingleAsync<User>(query);
 			return user;
 		}
@@ -74,14 +76,14 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 		{
 			var query = $"select top(1) " +
 				$"{_id}, {_email}, {_username}, {_password}, {_firstName}, {_lastName}, {_roleLevel} " +
-				$"from dbo.users where username = '{username}'";
+				$"from {_table} where username = '{username}'";
 			var user = await _context.GetSingleAsync<User>(query);
 			return user;
 		}
 
 		public async Task<bool> Update(User userUpdated)
 		{
-			var query = " update dbo.users set";
+			var query = $" update {_table} set";
 			if (userUpdated.Username != string.Empty) query += $" username = '{userUpdated.Username}',";
 			if (userUpdated.Email != string.Empty) query += $" email = '{userUpdated.Email}',";
 			if (userUpdated.FirstName != string.Empty) query += $" first_name = '{userUpdated.FirstName}',";
@@ -89,13 +91,13 @@ namespace BushidoAdministration.HourTicket.api.Repositories
 			query = query.Remove(query.Length - 1); //Per togliere la virgola
 			query += $" where id = {userUpdated.Id}";
 
-			return await _context.Update(query);
+			return await _context.UpdateAsync(query);
 		}
 
 		public async Task<bool> UpdatePassword(int userId, string oldPassword, string newPassword)
 		{
-			var query = $"update dbo.users set password = '{newPassword}' where id = {userId} and password = '{oldPassword}'";
-			return await _context.Update(query);
+			var query = $"update {_table} set password = '{newPassword}' where id = {userId} and password = '{oldPassword}'";
+			return await _context.UpdateAsync(query);
 		}
 
 		public async Task<bool> ExistsFromId(int userId)
