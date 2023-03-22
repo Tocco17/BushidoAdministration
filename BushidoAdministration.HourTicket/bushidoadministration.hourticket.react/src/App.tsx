b2c52cfea507b42e35e18fa9624eb21680641f66
FedericoRuocco <federico.ruocco@giuneco.it>
2023-03-22 09:19:53 +0100

    Test: theme

--- BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/App.tsx
+++ BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Routing from './components/routing';
+import { ThemeProvider } from '@emotion/react';
+import theme from './themes';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const customization = useSelector((state) => state.customization)
+
   return (
     <>
-    <Routing />
+    <ThemeProvider theme={theme(customization)}>
+
+      <Routing />
+    </ThemeProvider>
     </>
   );
 }
