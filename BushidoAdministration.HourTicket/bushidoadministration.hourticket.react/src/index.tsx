aed2cc3768bf61c51091f5199eacc09e66c90574
FedericoRuocco <federico.ruocco@giuneco.it>
2023-03-24 09:24:00 +0100

    Fix: cleaned all pages

--- BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/index.tsx
+++ BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/index.tsx
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AuthProvider from './stores/auth-provider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />}/>
-        </Routes>
-      </BrowserRouter>
-    {/* </AuthProvider> */}
+    <App />
   </React.StrictMode>
 );
--- BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/interfaces/Auth.ts
+++
@@ -1,9 +0,0 @@
-import Role from "../enums/role.enum"
-
-interface AuthInterface {
-    roles: Role[],
-    accessToken: string,
-    user: any
-}
-
-export default AuthInterface
