cdc13ae34fcde9c6388e98c845347010c6944039
FedericoRuocco <federico.ruocco@giuneco.it>
2023-03-24 09:59:23 +0100

    Create: created navbar component

--- BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/components/layouts/default-layout.tsx
+++ BushidoAdministration.HourTicket/bushidoadministration.hourticket.react/src/components/layouts/default-layout.tsx
@@ -1,26 +1,12 @@
 import { Link, Outlet } from "react-router-dom"
-import { allPath } from "../../utilities/routes/routes.utility"
+import { allPath, allRoutes } from "../../utilities/routes/routes.utility"
 import GoBackButton from "../buttons/go-back-button"
+import NavbarComponent from "../navbar/navbar"
 
 const DefaultLayout = () => {
     return (
         <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to={allPath.home}>Home</Link>
-                </li>
-                <li>
-                    <Link to={allPath.details}>Details</Link>
-                </li>
-                <li>
-                    <Link to={allPath.login}>Login</Link>
-                </li>
-                <li>
-                    <Link to={allPath.logout}>Logout</Link>
-                </li>
-            </ul>
-        </nav>
+        <NavbarComponent routes={[allRoutes.home, allRoutes.details]} settings={[allRoutes.login, allRoutes.logout]} />
 
         <GoBackButton label="Torna indietro"/>
 
